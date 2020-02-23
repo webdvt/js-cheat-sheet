4b5d42373ef984b9f5df15ff69468b18f6dae6eb
hassanyakef <yhassan@vt.edu>
2020-02-23 13:04:18 -0500

    Add a challenge :kissing_smiling_eyes:

---
+++ challenge.js
@@ -0,0 +1,73 @@
+// Can you solve these challenges 😉 ?
+
+
+
+// #1 Create a function that takes in a person's name and
+// console logs a greeting message
+// E.g. input: 'John' --> output: 'Hello John'
+
+
+
+
+// #2 Create a function that takes in an object (e.g. person) and outputs
+// a string using that object's properties.
+/* E.g. input:
+          person = {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 25
+
+        output: 'Hello, my name is John Doe and I am 25 years old.'
+ */
+
+
+
+// #3 Create a function that takes in an array of temperatures in fahrenheit
+// and return a new array with the temperatures in celsius.
+// Here is the conversion formula: T(°C) = (T(°F) - 32) × 5/9
+const temperaturesInF = [42, 53, 22, 60]; // input
+// output: [5.56, 11.67, -5.55, 15.66]
+
+
+
+// #4 Given an object of people and their ages, return how old the
+// people would be after n (positive number) years have passed.
+/* E.g.
+afterNYears({
+    "Joel" : 32,
+    "Fred" : 44,
+    "Reginald" : 65,
+    "Susan" : 33,
+    "Julian" : 13
+  }, 1) ➞ {
+    "Joel" : 33,
+    "Fred" : 45,
+    "Reginald" : 66,
+    "Susan" : 34,
+    "Julian" : 14
+  }
+*/
+
+
+
+
+
+// #5 Write a function that converts an object into an array, where each element represents a key-value pair.
+/* toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
+  toArray({ shrimp: 15, tots: 12 }) ➞ [["shrimp", 15], ["tots", 12]]
+  toArray({}) ➞ []
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-// String, Numbers, Boolean, null, undefined
-const name = 'John'; // String
-const age = 15; // Number
-const isLegal = false; // Boolean
-const uberRating = 3.3; // Number
+// --- Primitive data types ---
+// string, number, boolean, null, undefined
+const name = 'John'; // string
+const age = 27; // number
+const isMarried = false; // boolean
+const height = 5.6; // number
 const x = null; 
 const y = undefined;
-let z; // Undefined
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isLegal);
-console.log(typeof uberRating);
-console.log(typeof y);
+let z;
+
+// How to check the type of a variable?
+console.log('Type of z:', typeof z); // undefined
+console.log('Type of name:', typeof name);
+console.log('Type of age:', typeof age);
+console.log('Type of isMarried:', typeof isMarried);
+console.log('Type of height:', typeof height);
+console.log('Type of y:', typeof y);
+
+// There are 3 ways to declare variables --> const, let and var.
+
+// What is the difference between let and const?
+//    Use const when you know you will not reassign a variable,
+//    otherwise, use let.
+// DON't use var!
+const student1 = 'John';
+student1 = 'Jane'; // TypeError: Assignment to constant variable.
+
+let student2 = 'Sam';
+student2 = 'Jack'; // :)
+
+// --- String ---
 
 // String concatenation
 console.log("My name is " + name + " and I am " + age + " years old.");
@@ -19,45 +37,49 @@ console.log(`My name is ${name} and I am ${age} years old.`);
 
 // String operations and properties
 const str = "Hello World";
-console.log(`The length of s is ${str.length}`);
-console.log(str.substring(0, 5)); // From index 0 to index 4 (2nd parameter exclusive)
+console.log(`The length of str is: ${str.length}`);
+console.log('Str substring: ', str.substring(0, 5)); // From index 0 to index 4 (2nd parameter exclusive)
 let nums = "1,2,3,4,5";
 console.log(nums.split(',')); // Splits string by delimiter
 
-// Arrays
+// --- Arrays ---
 const integers = new Array(1, 2, 3, 4);
-console.log(integers);
+console.log({integers});
 const fruits = ['apples', 'oranges', 'bananas', 'mangoes'];
-console.log(fruits);
+console.table({fruits});
 console.log(`The length of fruits is ${fruits.length}`);
 const mixArr = ['1', 2, true]; // Array with different data types
-console.log(mixArr);
+console.log({mixArr});
 // Accessing elements in array
 console.log(fruits[1]); // index starts at 0
 // Add element to array if you know array size
 fruits[4] = 'grapes';
-console.log(fruits);
+console.log({fruits});
 // Add element to end of array if size is unknown
 fruits.push('pears');
-console.log(fruits);
+console.log({fruits});
 // fruits = []; is illegal since it is being assigned to a different array
 // Add element to beginning of array
 fruits.unshift('strawberries');
-console.log(fruits);
+console.log({fruits});
 // Remove last element in array
 fruits.pop();
-console.log(fruits);
+console.log({fruits});
 // Remove from beginning of array
 fruits.shift();
-console.log(fruits);
+console.log({fruits});
 // Remove element at specific index
 fruits.splice(0,0);
-console.log(fruits);
+console.log({fruits});
 // Remove range of elements
 fruits.splice(1, 3);
 console.log(fruits);
 
-// Object Literals
+// --- Object Literals ---
+
+// What is Object literal in JS?
+//      Object literal is a comma-separated list of name-value pairs
+//      wrapped in curly braces.
 let person = {
     firstName: 'John',
     lastName: 'Doe',
@@ -69,7 +91,7 @@ let person = {
         state: 'Virginia'
     }
 };
-console.log(person);
+console.log({person});
 // Access single value
 console.log(person.firstName);
 console.log(person.hobbies[2]);
@@ -81,7 +103,19 @@ console.log(firstName, lastName, state);
 person.email = 'johndoe@gmail.com';
 console.log(person);
 
-// For loops
+// --- Better way to console.log ---
+
+const foo  = 23;
+const bar = 24;
+// Bad way:
+console.log(foo, bar);
+// Better way:
+console.log({foo, bar});
+// Alternative way:
+console.table([foo, bar]);
+
+
+//  --- For loops ---
 const todos = [
     {
         id: 1,
@@ -99,64 +133,83 @@ const todos = [
         isComplete: false
     }
 ];
-// set iterator value, set bound, set increment value
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
+
 // Array traversal
 for (let i = 0; i < todos.length; i++) {
     console.log(todos[i].text);
 }
 
-// For Each
 for (let todo of todos) {
     console.log(todo.id);
 }
-todos.forEach((todo) => {
-    console.log(todo);
+
+// forEach
+todos.forEach(function(todo) {
+    console.log(todo.text);
 });
 
-// While loop
-let counter = 0; 
-while (counter < 10) {
-    console.log(counter * counter);
-    counter++;
+// Array map
+// Creates a new array A2 by calling a function on each element in the input array A1.
+const users = [
+    { firstName: 'John', lastName: 'Doe'},
+    { firstName: 'Sarah', lastName: 'Smith'},
+    { firstName: 'Sam', lastName: 'Williams'},
+];
+
+// Old school imperative approach: use statements to mutate (aka change) state
+const firstNames = [];
+
+for(const user of users) {
+    firstNames.push(user.firstName);
 }
 
-// Do while
-counter = 0;
-do {
-    console.log(counter * counter);
-    counter++;
-}
-while (counter < 10);
+console.table([firstName]);
 
-// Conditionals
-counter = 0;
-if (counter === 0) {
-    console.log('counter equals to 0');
-}
-else if (counter < 0) {
-    console.log('counter is negative');
-}
-else {
-    console.log('counter is positive');
-}
+// Declarative approach: use function to describe state
+const firstNames2 = users.map((user) => user.firstName);
+console.table([firstNames2]);
 
-// Functions
-function isEven(x) {
-    return x % 2 === 0;
-}
-console.log(isEven(4));
-console.log(isEven(3));
 
-const mean = (set) => {
-    let sum = 0;
-    for (let i = 0; i < set.length; i++) {
-        sum += set[i];
-    }
-    return sum / set.length;
+// How to merge arrays or object literal?
+const person1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 27
 };
 
-console.log(mean([1, 2, 3]));
-console.log(mean([3,5,3,2,3,4,5]));
+const job = {jobTitle: 'developer', company: 'companyX'};
+
+const person1Merged = {...person1, ...job};
+console.log({person1Merged});
+
+const tasks = ['do homework', 'grocery shopping', 'do laundry'];
+const tasks1 = ['take out the trash', ...tasks];
+const tasks2 = [...tasks, 'take out the trash'];
+console.log({tasks, tasks1, tasks2});
+
+// --- Double equal sign '==' vs triple equal sign '===' ---
+
+// '==' --> converts the type before running the comparison
+const x = '23' == 23;
+const y = '23' === 23;
+console.log({x, y});
+
+
+// --- Functions ---
+
+// 'function' syntax
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+// modern ES6 arrow syntax
+const isEven2 = (num) => {
+    return num % 2 === 0;
+};
+
+console.log('isEven():', isEven(4));
+console.log('isEven2():', isEven2(4));
+
+
+
+
